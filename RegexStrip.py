93dd6c596ad2f91e31a46bad8c2b1e721bdfdbb2
ieshaan12 <31623825+ieshaan12@users.noreply.github.com>
2019-01-15 18:08:29 +0530

    Python Programs

--- RegexStrip.py
+++ RegexStrip.py
@@ -1,13 +1,16 @@
 #! python
 # Regex Version of Strip()
 import re
-def RegexStrip(mainString,charsToBeRemoved):
+def RegexStrip(mainString,charsToBeRemoved=None):
     if(charsToBeRemoved!=None):
-        regex=re.compile(r'[%s]'%charsToBeRemoved)
+        regex=re.compile(r'[%s]'%charsToBeRemoved)#Interesting TO NOTE
         return regex.sub('',mainString)
     else:
-        regex=re.compile(r'\s+')
-        return regex.sub('',mainString)
+        regex=re.compile(r'^\s+')
+        regex1=re.compile(r'$\s+')
+        newString=regex1.sub('',mainString)
+        newString=regex.sub('',newString)
+        return newString
     
 Str='   hello3123my43name is antony    '
-print(RegexStrip(Str,None))
+print(RegexStrip(Str))
